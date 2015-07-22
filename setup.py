f71c9387bf707cb272169767a70b1cd75bfd8295
Karolis Rusenas <karolis.rusenas@opencredo.com>
2015-07-22 14:14:45 +0100

    cleanup setup.py

--- setup.py
+++ setup.py
@@ -7,56 +7,22 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.md')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
-# moving requirements to pip file
-# requires = [
-#     'tornado==4.1',
-#     'futures>=2.2.0',
-#     'redis',
-#     'pymongo==2.8',
-#     'statsd',
-#     'pytz',
-#     'requests>=2.4.3',
-#     'lxml',
-#     'jsonpath-rw',
-#     'python-dateutil>=2.2',
-#     'Pygments',
-#     'dogpile.cache',
-#     'matchers',
-#     'PyYAML',
-#     # testing
-#     'ming',
-#     'pylint',
-#     'coverage',
-#     'nose',
-#     'nosexcover',
-#     'mock',
-#     # performance profiling
-#     'plop',
-#     'yappi',
-#     # docs
-#     'Sphinx',
-#
-#     ]
-
-
 setup(name='stubo',
       version=".".join(map(str, version_info)),
       description='Stubo',
-      long_description=README + '\n\n' +  CHANGES,
+      long_description=README + '\n\n' + CHANGES,
       classifiers=[
-        "Programming Language :: Python",
-        "Topic :: Internet :: WWW/HTTP",
-        ],
+          "Programming Language :: Python",
+          "Topic :: Internet :: WWW/HTTP",
+      ],
       url='stubomatic.net',
       license='LICENSE.txt',
       keywords='Stub-O-Matic stubo tester testing service virtualisation',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
-      # install_requires = requires,
-      # tests_require= requires,
       test_suite="stubo",
-      entry_points = """\
+      entry_points="""\
       [console_scripts]
       stubo = stubo.scripts.service:run_stubo_app
       export_stubs = stubo.scripts.scenario:export_stubs
@@ -66,6 +32,5 @@ setup(name='stubo',
       delete_test_dbs = stubo.scripts.admin:delete_test_dbs  
       create_tracker_collection = stubo.scripts.admin:create_tracker_collection  
       purge_stubs = stubo.scripts.admin:purge_stubs  
-      """      
+      """
       )
-
