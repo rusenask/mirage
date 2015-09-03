4940a04f6eb0591fac82cdf2b0f2b9160bcca486
Karolis Rusenas <karolis.rusenas@opencredo.com>
2015-09-03 15:07:09 +0100

    pep8 exceptions

--- stubo/exceptions.py
+++ stubo/exceptions.py
@@ -2,18 +2,20 @@
     :copyright: (c) 2015 by OpenCredo.
     :license: GPLv3, see LICENSE for more details.
 """
+
+
 class StuboException(Exception):
     """ Base class for all :term:`exception response` objects."""
-    
+
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
-        
+
     def __str__(self):
         return "%s: %s: %s" % (self.code, self.title, self.explanation)
 
 
 ############################################################
-## 4xx client error
+# 4xx client error
 ############################################################
 class HTTPClientError(StuboException):
     """
@@ -28,15 +30,18 @@ class HTTPClientError(StuboException):
     title = 'Bad Request'
     explanation = ('The server could not comply with the request since '
                    'it is either malformed or otherwise incorrect.')
-    
+
+
 class UserExitModuleNotFound(HTTPClientError):
     pass
 
+
 class TransformError(HTTPClientError):
     pass
-        
+
+
 ############################################################
-## 5xx Server Error
+# 5xx Server Error
 ############################################################
 class HTTPServerError(StuboException):
     """
@@ -50,7 +55,8 @@ class HTTPServerError(StuboException):
     explanation = (
         'The server has either erred or is incapable of performing '
         'the requested operation.')
-        
+
+
 def exception_response(status_code, **kw):
     """Creates an HTTP exception based on a status code. Example::
 
@@ -62,9 +68,9 @@ def exception_response(status_code, **kw):
     if status_code > 499:
         exc = HTTPServerError(**kw)
     elif status_code > 399:
-        exc = HTTPClientError(**kw) 
+        exc = HTTPClientError(**kw)
     else:
         raise ValueError('status_code must be > 399, not {0}'.format(
-                                                                status_code))
+            status_code))
     exc.code = status_code
     return exc
