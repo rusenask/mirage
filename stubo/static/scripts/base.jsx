f98f989a3ecc649b2b3d0b56744af24e96d899e8
Karolis Rusenas <karolis.rusenas@gmail.com>
2015-09-23 17:58:35 +0100

    implementing "full tracking level checkbox

--- stubo/static/scripts/base.jsx
+++ stubo/static/scripts/base.jsx
@@ -1,4 +1,6 @@
 var React = require('../node_modules/react');
+var Input = require('../node_modules/react-bootstrap').Input;
+
 
 function getBooleanState(trackingLevel) {
     return trackingLevel == "full";
@@ -18,28 +20,50 @@ var TrackingLevelComponent = React.createClass({
         // fetching data
         $.get(this.state.apiHref, function (result) {
             var trackingState = result.data.tracking_level;
-            console.log(trackingState);
             // update state
             if (this.isMounted()) {
                 this.setState({
                     checked: getBooleanState(trackingState)
                 });
-
-
             }
         }.bind(this));
     },
 
+    handleClick: function () {
+        // getting current state
+        var fullTrack = this.state.checked;
+
+        var settingValue = "full";
+        if (fullTrack) {
+            settingValue = "normal";
+        }
+        // creating uri to change current state
+        var uri = "/stubo/api/put/setting?setting=tracking_level&value=" + settingValue;
+
+        $.get(uri, function (result) {
+            var trackingState = result.data.tracking_level;
+            // update state
+            if (this.isMounted()) {
+                this.setState({
+                    checked: getBooleanState(trackingState)
+                });
+            }
+        }.bind(this));
+
+    },
+
     render: function () {
+        // rendering field with checkbox
         return (
-            <a href="#" id="all_hosts_label">
+            <a href="#" onClick={this.handleClick}>
                 <i className="fa fa-flash"></i>
                 <span> Full tracking level </span>
-                <input type="checkbox"
-                       className="pull-right" value={this.state.checked}/>
+                <input className="pull-right"
+                       type="checkbox"
+                       checked={this.state.checked}
+                       onChange={this.handleClick}/>
             </a>
         )
-
     }
 
 });
