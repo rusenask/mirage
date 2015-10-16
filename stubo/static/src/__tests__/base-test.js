2a640b75aa16d48ec4cfe21612fbc8ccbea32055
Karolis Rusenas <karolis.rusenas@gmail.com>
2015-10-16 13:28:24 +0100

    initial test for base jsx

--- stubo/static/src/__tests__/base-test.js
+++ stubo/static/src/__tests__/base-test.js
@@ -1,11 +1,11 @@
 var React = require('react');
 var TestUtils = require('react/lib/ReactTestUtils'); //I like using the Test Utils, but you can just use the DOM API instead.
 var expect = require('expect');
-var Root = require('../root'); //my root-test lives in components/__tests__/, so this is how I require in my components.
+var TrackingLevelComponent = require('../base'); //my root-test lives in components/__tests__/, so this is how I require in my components.
 
-describe('root', function () {
+describe('TrackingLevelComponent exists', function () {
     it('renders without problems', function () {
-        var root = TestUtils.renderIntoDocument(<Root/>);
-        expect(root).toExist();
+        var TrackingLevelComponent = TestUtils.renderIntoDocument(<TrackingLevelComponent/>);
+        expect(TrackingLevelComponent).toExist();
     });
 });
